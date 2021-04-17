19915d7590706c73ee49883211255fc40e8e74c2
矢野文久 <fumiblog@yanobunkyuunoMacBook-Pro.local>
2021-04-18 08:41:46 +0900

    [customer_users] 2-10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_234358) do
+ActiveRecord::Schema.define(version: 2021_04_17_061648) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_04_08_234358) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "department"
+    t.string "name"
   end
 
   create_table "customers", force: :cascade do |t|
